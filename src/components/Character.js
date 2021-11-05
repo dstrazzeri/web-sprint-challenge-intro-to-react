553c2945b6a2c358c61615137e3bae9b7afa236d
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2021-11-05 15:06:41 -0400

    added birth year and some styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,23 +3,26 @@ import styled from 'styled-components'
 
 
 const StyledChar = styled.div`
-border: 2px solid red;
+border: 3px solid #14213d;
 background: rgba(19, 32, 24, 0.7);
-margin-left: 15%;
-margin-right: 15%;
+border-radius: 1%;
+margin: auto 20%;
 padding: 3%;
+display: flex;
+justify-content: center;
 `
 
 const StyledPer = styled.div`
 color: #ffffff;
-border: 5px dotted red;
-border-radius: 40%;
+border: 5px dotted #14213d;
 box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-margin-left: 20%;
-margin-right: 20%;
 margin-bottom: 10%;
 padding: 5% 10%;
-font-size: 2em;
+font-size: 3em;
+`
+const StyledYear = styled.h2`
+color:white;
+font-size: 1.5rem;
 `
 
 const Character = (props) => {
@@ -30,9 +33,12 @@ const Character = (props) => {
         <div> 
         {character.map((res) => {
             return (
+                <div>
                 <StyledPer>
-                {res.name}
+                {res.name}<br />
+                <StyledYear>Birth Year: {res.birth_year}</StyledYear>                
                 </StyledPer>
+                </div>
             )
         })}
         </div>
