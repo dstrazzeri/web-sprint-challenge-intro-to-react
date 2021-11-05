838823c37f0bf4100f2c9f68fe25a7a549e049b2
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2021-11-05 14:07:00 -0400

    characters returning to dom, working on styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,23 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [charData, setCharData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => {
+      console.log(res.data);
+      return setCharData(res.data);
+    })
+    .catch(err => {
+      console.log('These are not the droids you are looking for')
+    })
+  }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">REACT WARS</h1>
+      <Character character={charData} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,43 @@
-// Write your Character component here
+import React from "react";
+import styled from 'styled-components'
+
+
+const StyledChar = styled.div`
+border: 2px solid red;
+background: rgba(19, 32, 24, 0.7);
+margin-left: 15%;
+margin-right: 15%;
+padding: 3%;
+`
+
+const StyledPer = styled.div`
+color: #ffffff;
+border: 5px dotted red;
+border-radius: 40%;
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+margin-left: 20%;
+margin-right: 20%;
+margin-bottom: 10%;
+padding: 5% 10%;
+font-size: 2em;
+`
+
+const Character = (props) => {
+    const {character} = props;
+    
+    return (
+    <StyledChar>
+        <div> 
+        {character.map((res) => {
+            return (
+                <StyledPer>
+                {res.name}
+                </StyledPer>
+            )
+        })}
+        </div>
+    </StyledChar>
+  )
+}
+
+export default Character;
